5800f37b6e56362fb6e8008c1f8677dc73eabb0a
timjlowe <tim.lowe@consensys.net>
2018-06-18 10:12:16 +0100

    Update apiKeys.py

--- apiKeys.py
+++ apiKeys.py
@@ -6,7 +6,7 @@ def getTransportApiKey():
 	api_key = {}
 	if (app.config['USE_TIMS_API_KEYS'] == True):
 		print ('Using Tims API Keys')
-		api_key = {'app_id' : 'bdd013aa', 'key' : 'd3c6ec9a4b3b831510289fe15d36abf4'}
+		api_key = {'app_id' : '', 'key' : ''}
 	else:
 		api_key = {'app_id' : '03bf8009', 'key' : 'd9307fd91b0247c607e098d5effedc97'}
 	
